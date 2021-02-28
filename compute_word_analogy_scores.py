9f72ecd014d70ffff15c84ab765edc5100a02afd
akashsara <akashsara@outlook.com>
2021-02-28 17:39:16 +0530

    Word Analogy Notebook -> Script

---
+++ compute_word_analogy_scores.py
@@ -0,0 +1,42 @@
+import numpy as np
+from gensim.scripts import glove2word2vec
+from gensim.models.keyedvectors import Word2VecKeyedVectors
+
+# Reproducibility
+seed = 42
+np.random.seed(seed)
+
+### USER INPUT ###
+experiment_name = "pca_v1"
+test_data_path = "data/datasets/questions-words.txt"
+### END USER INPUT ###
+
+input_file = f"data/embeddings/trained/{experiment_name}.glove.6B.300d.txt"
+converted_file = f"data/embeddings/trained/{experiment_name}.glove.gensimFormat.6B.300d.txt"
+
+# Convert GloVe Vectors into a format usable by gensim
+_ = glove2word2vec.glove2word2vec(input_file, converted_file)
+print(f"Converted to Gensim Format: {_}")
+
+glove = Word2VecKeyedVectors.load_word2vec_format(converted_file)
+
+print("Evaluating...")
+results = glove.evaluate_word_analogies(test_data_path, restrict_vocab=400000)
+
+syn_correct = 0
+syn_incorrect = 0
+sem_correct = 0
+sem_incorrect = 0
+for item in results[1]:
+    correct = len(item['correct'])
+    incorrect = len(item['incorrect'])
+    if item['section'] == "Total accuracy":
+        print(f"{item['section']}: {correct * 100/(correct+incorrect)}")
+    elif item['section'][:4] == 'gram':
+        sem_correct += correct
+        sem_incorrect += incorrect
+    else:
+        syn_correct += correct
+        syn_incorrect += incorrect
+print(f"Semantic Accuracy: {sem_correct * 100/(sem_correct+sem_incorrect)}")
+print(f"Syntactic Accuracy: {syn_correct * 100/(syn_correct+syn_incorrect)}")
