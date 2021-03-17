69b58eaaebbdbf8e5b7955fb2e30bbb4c2ce8b77
akashsara <akashsara@outlook.com>
2021-03-17 22:48:56 +0530

    add bulk mode to script

--- compute_word_analogy_scores.py
+++ compute_word_analogy_scores.py
@@ -4,45 +4,72 @@ from gensim.models.keyedvectors import Word2VecKeyedVectors
 import os
 import sys
 
+
+def get_gensim_vectors(input_file, converted_file):
+    # Convert GloVe vectors into a format usable by gensim
+    _ = glove2word2vec.glove2word2vec(input_file, converted_file)
+    # Load converted vectors
+    return Word2VecKeyedVectors.load_word2vec_format(converted_file)
+
+
+def print_results(results):
+    syn_correct = 0
+    syn_incorrect = 0
+    sem_correct = 0
+    sem_incorrect = 0
+    for item in results[1]:
+        correct = len(item["correct"])
+        incorrect = len(item["incorrect"])
+        if item["section"] == "Total accuracy":
+            print(f"{item['section']}: {correct * 100/(correct+incorrect)}")
+        elif item["section"][:4] == "gram":
+            sem_correct += correct
+            sem_incorrect += incorrect
+        else:
+            syn_correct += correct
+            syn_incorrect += incorrect
+    print(f"Semantic Accuracy: {sem_correct*100/(sem_correct+sem_incorrect)}")
+    print(f"Syntactic Accuracy: {syn_correct*100/(syn_correct+syn_incorrect)}")
+
+
 # Reproducibility
 seed = 42
 np.random.seed(seed)
 
 ### USER INPUT ###
-input_file = sys.argv[1]
-converted_file = input_file + "2"
+path_to_dir_or_file = sys.argv[1]
+mode = sys.argv[2]
 test_data_path = "data/datasets/questions-words.txt"
 ### END USER INPUT ###
 
+if mode == "bulk":
+    print("Running in BULK mode.")
+    list_of_files = os.listdir(path_to_dir_or_file)
+    for input_file in list_of_files:
+        print("\n")
+        print(input_file)
+        input_file = os.path.join(path_to_dir_or_file, input_file)
+        converted_file = input_file + "2"
+        glove = get_gensim_vectors(input_file, converted_file)
 
-# Convert GloVe vectors into a format usable by gensim
-_ = glove2word2vec.glove2word2vec(input_file, converted_file)
-print(f"Converted to Gensim Format: {_}")
+        # Evaluate
+        results = glove.evaluate_word_analogies(test_data_path, restrict_vocab=400000)
 
-# Load converted vectors
-glove = Word2VecKeyedVectors.load_word2vec_format(converted_file)
+        # Print Results
+        print_results(results)
 
-# Evaluate & Print
-print("Evaluating...")
-results = glove.evaluate_word_analogies(test_data_path, restrict_vocab=400000)
+        # Remove converted vectors file
+        os.remove(converted_file)
+else:
+    converted_file = path_to_dir_or_file + "2"
+    glove = get_gensim_vectors(path_to_dir_or_file, converted_file)
 
-syn_correct = 0
-syn_incorrect = 0
-sem_correct = 0
-sem_incorrect = 0
-for item in results[1]:
-    correct = len(item['correct'])
-    incorrect = len(item['incorrect'])
-    if item['section'] == "Total accuracy":
-        print(f"{item['section']}: {correct * 100/(correct+incorrect)}")
-    elif item['section'][:4] == 'gram':
-        sem_correct += correct
-        sem_incorrect += incorrect
-    else:
-        syn_correct += correct
-        syn_incorrect += incorrect
-print(f"Semantic Accuracy: {sem_correct * 100/(sem_correct+sem_incorrect)}")
-print(f"Syntactic Accuracy: {syn_correct * 100/(syn_correct+syn_incorrect)}")
+    # Evaluate
+    print("Evaluating...")
+    results = glove.evaluate_word_analogies(test_data_path, restrict_vocab=400000)
 
-# Remove converted vectors file
-os.remove(converted_file)
+    # Print Results
+    print_results(results)
+
+    # Remove converted vectors file
+    os.remove(converted_file)
